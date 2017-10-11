6ae6353649a4cd9305ff2ceff444d9728e8008c3
Mike Lambert <lambert8@illinois.edu>
2017-10-11 11:33:23 -0500

    This was missing from the PR for some reason

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 
-#if [ ! -d ~/work/data ]; then
-#   ln -s /data ~/work/
-#fi 
-#
-#if [ ! -d ~/work/tutorials ]; then
-#   git clone https://github.com/terraref/tutorials.git ~/work/tutorials
-#fi 
+BASHRC_TEMPLATE_PATH="/etc/skel/.bashrc"
+BASHRC_PATH="$HOME/.bashrc"
 
+# If user doesn't already have a local .bashrc file
+if [ ! -f "$BASHRC_PATH" ]; then
+    # Copy the skeleton .bashrc file
+    echo "Copying base .bashrc file to $BASHRC_PATH"
+    cp $BASHRC_TEMPLATE_PATH $BASHRC_PATH
+
+    # Set an alias for python2 to use that by default instead
+    echo ""
+    echo "# Comment this line and restart shell to use Python 3" >> $BASHRC_PATH
+    echo "alias python='python2'" >> $BASHRC_PATH
+fi
 
 . /usr/local/bin/start.sh jupyter notebook $*
