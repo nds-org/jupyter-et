56f2c46a0d91e945f946d54992d440b304df2d20
Steven R. Brandt <sbrandt@cct.lsu.edu>
2021-07-21 18:10:09 +0000

    Updates to make things smoother for the workshop. Includes a prebuilt Cactus.

---
+++ tutorial-server/build-cactus-tarball.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ex
+
+NCPUS=$(lscpu|grep '^CPU.s.:'|cut -d: -f2)
+
+if [ "$USER" = "" ]
+then
+    export USER=etuser
+    export HOME=/usr/etuser
+fi
+
+cd
+
+# BUILD CACTUS
+curl -kLO https://raw.githubusercontent.com/gridaphobe/CRL/ET_2021_05/GetComponents
+chmod a+x GetComponents
+./GetComponents --parallel https://bitbucket.org/einsteintoolkit/manifest/raw/ET_2021_05/einsteintoolkit.th
+cd ~/Cactus
+./simfactory/bin/sim setup-silent
+./simfactory/bin/sim build -j$(($NCPUS/2)) --thornlist ../einsteintoolkit.th
+
+ls ./exe/cactus_sim
+
+# MAKE TARBALL
+SED_IN_PLACE='t=$(stat -c %y "$0"); sed -i "s!'$PWD'!\$(CCTK_HOME)!g" "$0"; touch -d "$t" "$0"'
+
+find configs/sim -name \*.d -exec bash -c "$SED_IN_PLACE" '{}' \;
+find configs/sim -name \*.ccldeps -exec bash -c "$SED_IN_PLACE" '{}' \;
+find configs/sim -name \*.deps -exec bash -c "$SED_IN_PLACE" '{}' \;
+find configs/sim -name \*.defn -exec bash -c "$SED_IN_PLACE" '{}' \;
+
+tar --exclude etk1.cct.lsu.edu.ini --exclude defs.local.ini -czf ../${PWD##*/}.tar.gz ../${PWD##*/}
+
+cd
+rm -fr Cactus CactusSourceJar.git  GetComponents  einsteintoolkit.th
--- tutorial-server/startup.sh
+++ tutorial-server/startup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Make sure cron is running
+randpass MND | grep pass: | cut -f2 -d: | sed 's/\s//g' > /usr/enable_mkuser
+echo "STARTUP CODE:"
+cat /usr/enable_mkuser
+
 /etc/init.d/cron start >/dev/null 2>&1
 cron /root/crontab.txt
 cd /
+
 python3 /usr/local/bin/make_users.py
-jupyterhub --ip 0.0.0.0 --port 443 -f jup-config.py > /var/log/jup-log.txt 2>&1
+if [ -r /home/shadow ]
+then
+    cp /home/shadow /etc/shadow
+fi
+
+jupyterhub --ip 0.0.0.0 --port 443 -f jup-config.py 2>&1 | tee /var/log/jup-log.txt
