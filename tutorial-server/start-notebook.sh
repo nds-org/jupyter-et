3fe395b6ca2c223cbbbfbb5efaef1ab91a1c7cb9
Roland Haas <rhaas@illinois.edu>
2022-08-03 18:38:50 -0500

    tutorial-server: restore notebooks from /etc/skel if misisng

--- tutorial-server/start-notebook.sh
+++ tutorial-server/start-notebook.sh
@@ -6,6 +6,9 @@ set -e
 cd
 export PORT=8888
 SECRET_TOKEN=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
+echo "Copying missing notebooks"
+cp -van /etc/skel/*.ipynb ~/
+
 echo
 echo "To access the notebook, open this file in a browser copy and paste this URL:"
 echo
